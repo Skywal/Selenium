05f441b76c26448887b5bb9e1d05dc173096b46d
Skywal <Skaywoker6@gmail.com>
2018-12-08 08:02:39 +0200

    Add new class for string constants

---
+++ src/main/java/Final/Strings.java
@@ -0,0 +1,17 @@
+package Final;
+
+public class Strings {
+    //region URL
+    /**
+     * Body of URL for Longman dictionary english-english part
+     */
+    public final static String URL_BASE_DICTIONARY = "https://www.ldoceonline.com/dictionary/";
+    //endregion
+
+    //region File source
+    public final static String PATH_WORDS_TO_WORK_WITH = "source/sourceWords.txt";
+    //endregion
+    
+    //region Elements class names
+    //endregion
+}
