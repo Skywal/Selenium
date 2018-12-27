7e5f13a2658a9456b42762f9fcee4fd873accf7f
Skywal <Skaywoker6@gmail.com>
2018-12-27 15:27:36 +0200

    Make class WebContentParser

--- src/main/java/Content/WebContentParser.java
+++ src/main/java/Content/WebContentParser.java
@@ -3,13 +3,37 @@ package Content;
 import java.util.ArrayList;
 
 public class WebContentParser {
-    ArrayList<DictionaryItem> wordsList;
+    //region fields
+    /**
+     * full list of words from the web-dictionary
+     */
+    ArrayList<DictionaryItem> wordsList; //full list of words from the web-dictionary
+    //endregion
 
+    //region get/set
+    public ArrayList<DictionaryItem> getWordsList() {
+        return wordsList;
+    }
+    //endregion
+
+    //region construct
     public WebContentParser(){
+        initialize();
+    }
+
+    private void initialize(){
         wordsList = new ArrayList<DictionaryItem>();
     }
+    //endregion
 
-    public void addNewWord(String word, String trnascriptionEn){
-        wordsList.add(new DictionaryItem());
+    //region public
+    public void addNewWord(String word, String transcriptionEn, String transcriptionUs, String example){
+        wordsList.add(new DictionaryItem(
+                word,
+                transcriptionEn,
+                transcriptionUs,
+                example
+        ));
     }
+    //endregion
 }
